3842b33eff9902311ea60aa35b86eb028f159222
cfair <christopherpfair@comcast.net>
2017-05-28 09:04:41 -0600

    finished simple dungeon generator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 extern crate tcod;
+extern crate rand;
 
+
+use std::cmp;
 use tcod::console::*;
 use tcod::colors::{self, Color};
+use rand::Rng;
 
+// Actual size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
-
+// Size of the map in the window
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
+
+// Parameters for the autodungeon generator
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 const LIMIT_FPS: i32 = 20;
 
@@ -27,7 +37,6 @@ struct Tile {
 
 }
 
-
 impl Tile {
     pub fn empty() -> Self {
         Tile {blocked: false, block_sight: false}
@@ -37,6 +46,35 @@ impl Tile {
         Tile {blocked: true, block_sight: true}
     }
 }
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+
+impl Rect {
+    pub fn new(x: i32, y:i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x=(self.x1 + self.x2) / 2;
+        let center_y=(self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
+            (self.y1 <= other.y2) && (self.y2 >= other.y1)
+    }
+}
+
+
 
 
 #[derive(Debug)]
@@ -67,6 +105,7 @@ impl Object {
             self.y += dy;
         }
     }
+
     pub fn draw(&self, con: &mut Console)
     {
         con.set_default_foreground(self.color);
@@ -77,19 +116,71 @@ impl Object {
     {
         con.put_char(self.x, self.y, ' ', BackgroundFlag::None)
     }
-
-
-
 }
 
-fn make_map() -> Map {
-    let mut map  = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-
-    map [30][22] = Tile::wall();
-    map [50][22] = Tile::wall();
-
-    map
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
 }
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..cmp::max(x1, x2) + 1 {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(y1, y2)..cmp::max(y1, y2) + 1 {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map() -> (Map, (i32, i32)) {
+    let mut map  = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let mut rooms = vec![];
+
+    let mut starting_position = (0,0);
+
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+
+        let x  = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            create_room(new_room, &mut map);
+
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                starting_position = (new_x, new_y);
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            rooms.push(new_room);
+
+        }
+    }
+
+    (map, starting_position)
+}
+
 
 
 fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
@@ -150,16 +241,17 @@ fn main (){
         .title("Dungeon Crawler")
         .init();
 
+    tcod::system::set_fps(LIMIT_FPS);
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
-    tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(SCREEN_WIDTH /2, SCREEN_HEIGHT /2, '@', colors::WHITE);
+    let (map, (player_x, player_y)) = make_map();
+    
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH /2  -5, SCREEN_HEIGHT /2, '@', colors::YELLOW);
     let mut objects = [player, npc];
 
 
-    let map = make_map();
 
     while !root.window_closed() {
         render_all(&mut root, &mut con, &objects, &map);
