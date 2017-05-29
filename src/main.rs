5126cb1fee024c7c28a57a7cf9b590e7d8ddba5b
cfair <christopherpfair@comcast.net>
2017-05-28 19:06:14 -0600

    moved all non main code out of main.rs and into utilities.rs

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ mod map;
 mod rect;
 mod tile;
 mod object;
+mod utilities;
 
 use tcod::console::*;
 use tcod::colors::{self, Color};
 use map::make_map;
 use map::Map;
 use object::Object;
+use utilities::{handle_keys, render_all} ;
 
 // Actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -31,59 +33,6 @@ const LIMIT_FPS: i32 = 20;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100};
 const COLOR_DARK_GROUND: Color = Color {r: 50, g:50, b: 150};
-
-
-
-
-
-fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
-
-    for y in 0..MAP_HEIGHT {
-        for x in 0..MAP_WIDTH {
-            let wall  = map[x as usize][y as usize].block_sight;
-            if wall {
-                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
-        }
-    }
-
-    for object in objects {
-        object.draw(con);
-    }
-
-    blit (con, (0,0), (MAP_WIDTH, MAP_HEIGHT), root, (0,0), 1.0, 1.0);
-}
-
-
-
-fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
-    use tcod::input::Key;
-    use tcod::input::KeyCode::*;
-
-
-    let key = root.wait_for_keypress(true);
-    match key {
-
-        Key { code: Enter, alt: true, .. } => {
-            let fullscreen = root.is_fullscreen();
-            root.set_fullscreen(!fullscreen);
-        }
-
-
-        Key { code: Escape, ..} => return true,
-
-        Key { code: Up, ..} => player.move_by(0, -1, map),
-        Key { code: Down, .. } => player.move_by(0, 1, map),
-        Key { code: Left, .. } => player.move_by(-1, 0, map),
-        Key { code: Right, .. } => player.move_by(1, 0, map),
-
-        _ => {},
-    }
-
-    false
-}
 
 
 fn main (){
---
+++ src/utilities.rs
@@ -0,0 +1,55 @@
+use tcod::console::*;
+use tcod::colors::{self, Color};
+use tcod::Console;
+use object::Object;
+use map::Map;
+use tcod::console::blit;
+
+pub fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+
+    for y in 0..::MAP_HEIGHT {
+        for x in 0..::MAP_WIDTH {
+            let wall  = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, ::COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, ::COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit (con, (0,0), (::MAP_WIDTH, ::MAP_HEIGHT), root, (0,0), 1.0, 1.0);
+}
+
+
+pub fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+
+    let key = root.wait_for_keypress(true);
+    match key {
+
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+
+
+        Key { code: Escape, ..} => return true,
+
+        Key { code: Up, ..} => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
+
+        _ => {},
+    }
+
+    false
+}
+
